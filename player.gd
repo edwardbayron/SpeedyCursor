b2f66a8b324d4ec856d4cfbb9cf93f8278cdd75c
edwardbayron <edwardbayron@gmail.com>
2023-12-02 14:13:41 +0200

    set proper borders, where out from Player doesn't go
    it's coded in if else, where setting position. velocity and translate wen out of if else condition. now it moves constantly, but only in this area and by position it doesn't go out

--- player.gd
+++ player.gd
@@ -12,6 +12,9 @@ var distance_to_spawn_line = 100
 var player_movement_area
 var screen_size
 var new_position
+var stopped_at_border
+var top_y
+var bottom_y
 
 signal speed_counter(speed)
 signal background_speed_increased(speed)
@@ -20,7 +23,14 @@ signal spawn_line
 
 func _ready():
 	player_movement_area = $PlayerMovementArea/PlayerMovementCollision
+	top_y = position.y - player_movement_area.shape.extents.y
+	bottom_y = position.y + player_movement_area.shape.extents.y
 	screen_size = get_viewport_rect().size
+	
+	
+	print("top_y: "+str(top_y))
+	print("bottom_y: "+str(bottom_y))
+	print("screen size: "+str(screen_size.y))
 
 func _process(delta):
 	var rotation_in_radians = deg_to_rad(rotation)
@@ -69,25 +79,35 @@ func _process(delta):
 	else:
 		$AnimatedSprite2D.stop()
 	
+	print("self position y: "+str(self.position.y))
+	print("player position in area: "+str(self.position.y - top_y))
+	
+	
 	if is_within_player_movement_area(new_position):
 		position = new_position
-		if speed < 0:
-			velocity.y -= 1
-		velocity = velocity.normalized() * speed
-		translate(velocity * delta)
 	else:
-		position = player_movement_area.global_position - new_position - delta * velocity
+		if self.position.y <= top_y + 30:
+			print("STOP TOP")
+			position.y = player_movement_area.global_position.y - player_movement_area.shape.extents.y - 30
+			
+	
+		elif self.position.y >= bottom_y - 30:
+			print("STOP BOTTOM")
+			position.y = player_movement_area.global_position.y + player_movement_area.shape.extents.y + 30
 	
 #	if distance_traveled != null and distance_traveled >= distance_to_spawn_line:
 #		emit_signal("spawn_line")
 #		distance_traveled = 0
+	
+	velocity = velocity.normalized() * speed
+	translate(velocity * delta)	
 	
 	emit_signal("speed_counter", speed)
 	if position.y > 0:
 		emit_signal("background_speed_increased", speed)
 
 func is_within_player_movement_area(position):
-	return player_movement_area.global_position.distance_to(position) <= player_movement_area.shape.extents.length()
+	return player_movement_area.global_position.distance_to(position) < player_movement_area.shape.extents.length()
 
 func _on_player_movement_area_area_entered(area):
 	print("entered")
